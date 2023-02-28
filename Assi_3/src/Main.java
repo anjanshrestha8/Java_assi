005771bb865ecb17152d1ba4f5fd1f5f4f283386
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-02-28 09:22:38 +0545

    modified guessing number

--- Assi_3/src/Main.java
+++ Assi_3/src/Main.java
@@ -15,10 +15,10 @@ public class Main {
         int userChoice;
         char a = 'y';
         Scanner inputObj = new Scanner(System.in);
-        int i = 0, count = 0;
+        int i = 0, count = 5;
         while (a == 'y') {
-        System.out.println(ANSI_CYAN + "\n\n⚙︎⚙︎⚙︎⚙︎⚙︎" + ANSI_RESET + " Choose the level " + ANSI_CYAN + "︎︎⚙︎⚙︎⚙︎⚙︎" + ANSI_RESET);
-        System.out.println("\n---1.Easy    (1 to 10)\n---2.Medium  (1 to 50)\n---3.Hard    (1 to 100)");
+            System.out.println(ANSI_CYAN + "\n\n⚙︎⚙︎⚙︎⚙︎⚙︎" + ANSI_RESET + " Choose the level " + ANSI_CYAN + "︎︎⚙︎⚙︎⚙︎⚙︎" + ANSI_RESET);
+            System.out.println("\n---1.Easy    (1 to 10)\n---2.Medium  (1 to 50)\n---3.Hard    (1 to 100)");
             int n = inputObj.nextInt();
 
             switch (n) {
@@ -30,83 +30,110 @@ public class Main {
                     System.out.println(level1);
                     System.out.println("Computer has generated a number, You have 5 chance to guess it.");
                     for (i = 0; i <= 4; i++) {
-                        count++;
-                        System.out.println(" Try to guess your number from 0 to 10.......Chance "+(i+1));
+                        // count++;
+                        System.out.println(" Try to guess your number from 0 to 10.......Chance " + count);
+                        count--;
                         userChoice = inputObj.nextInt();
 
                         if (userChoice > level1) {
-                            System.out.println(ANSI_RED + userChoice + " is too high, please guess Lower number.." + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_RED + userChoice + " is too high, please guess Lower number.." + ANSI_RESET);
+                            }
                         } else if (userChoice < level1) {
-                            System.out.println(ANSI_RED + userChoice + " is too low, please guess Higher number.." + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_RED + userChoice + " is too low, please guess Higher number.." + ANSI_RESET);
+                            }
                         } else {
-                            System.out.println(ANSI_GREEN + "You have guessed it right..\uD83C\uDFC6\uD83C\uDFC6\uD83C\uDFC6" + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_GREEN + "You have guessed it right..\uD83C\uDFC6\uD83C\uDFC6\uD83C\uDFC6" + ANSI_RESET);
+                            }
                             break;
                         }
-                        if (count == 5) {
-                            System.out.println("Better luck next time.");
 
-                        }
+                    }
+                    if (count == 0) {
+                        System.out.println("Better luck next time.");
+
                     }
                     break;
 
-                    // level 2 (1 to 50 )
+                // level 2 (1 to 50 )
 
                 case 2:
                     Random random1 = new Random();
                     int level2 = random1.nextInt(50);
                     System.out.println(level2);
-                    count++;
-                    System.out.println("Computer has generated a number now its ur turn to guess it.");
+                    System.out.println("Computer has generated a number, You have 5 chance to guess it.");
                     for (i = 0; i <= 4; i++) {
-                        System.out.println("Guess your number from 1 to 50......Chance "+(i+1));
+                        // count++;
+                        System.out.println(" Try to guess your number from 1 to 50.......Chance " + count);
+                        count--;
                         userChoice = inputObj.nextInt();
+
                         if (userChoice > level2) {
-                            System.out.println(ANSI_RED + userChoice + " is too high, please guess Lower number.." + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_RED + userChoice + " is too high, please guess Lower number.." + ANSI_RESET);
+                            }
                         } else if (userChoice < level2) {
-                            System.out.println(ANSI_RED + userChoice + " is too low, please guess Higher number.." + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_RED + userChoice + " is too low, please guess Higher number.." + ANSI_RESET);
+                            }
                         } else {
-                            System.out.println(ANSI_GREEN + "You have guessed it right..\uD83C\uDFC6\uD83C\uDFC6\uD83C\uDFC6" + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_GREEN + "You have guessed it right..\uD83C\uDFC6\uD83C\uDFC6\uD83C\uDFC6" + ANSI_RESET);
+                            }
                             break;
-                        }
-                        if (count == 5) {
-                            System.out.println("Better luck next time.");
                         }
 
                     }
+                    if (count == 0) {
+                        System.out.println("Better luck next time.");
+
+                    }
                     break;
-                    // level 3 (1 to 100)
+
+                // level 3 (1 to 100)
 
                 case 3:
                     Random random2 = new Random();
                     int level3 = random2.nextInt(100);
                     System.out.println(level3);
-                    count++;
-                    System.out.println("Computer has generated a number now its ur turn to guess it.");
+                    System.out.println("Computer has generated a number, You have 5 chance to guess it.");
                     for (i = 0; i <= 4; i++) {
-                        System.out.println("Guess your number from 1 to 100.....Chance "+(i+1));
+                        // count++;
+                        System.out.println(" Try to guess your number from 1 to 100.......Chance " + count);
+                        count--;
                         userChoice = inputObj.nextInt();
+
                         if (userChoice > level3) {
-                            System.out.println(ANSI_RED + userChoice + " is too high, please guess Lower number.." + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_RED + userChoice + " is too high, please guess Lower number.." + ANSI_RESET);
+                            }
                         } else if (userChoice < level3) {
-                            System.out.println(ANSI_RED + userChoice + " is too low, please guess Higher number.." + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_RED + userChoice + " is too low, please guess Higher number.." + ANSI_RESET);
+                            }
                         } else {
-                            System.out.println(ANSI_GREEN + "You have guessed it right..\uD83C\uDFC6\uD83C\uDFC6\uD83C\uDFC6" + ANSI_RESET);
+                            if (count != 0) {
+                                System.out.println(ANSI_GREEN + "You have guessed it right..\uD83C\uDFC6\uD83C\uDFC6\uD83C\uDFC6" + ANSI_RESET);
+                            }
                             break;
                         }
-                        if (count == 5) {
-                            System.out.println("Better luck next time.");
 
-                        }
                     }
-                    break;
+                    if (count == 0) {
+                        System.out.println("Better luck next time.");
 
+                    }
+
+
+                    System.out.println("Do you want to play the game again. Press 'y' to play tha game again and 'n' to terminate the game.");
+                    a = inputObj.next().charAt(0);
             }
-            System.out.println("Do you want to play the game again. Press 'y' to play tha game again and 'n' to terminate the game.");
-            a= inputObj.next().charAt(0);
         }
-    }
 
     }
+}
 
 
 
