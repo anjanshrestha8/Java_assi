074709f2c9f1d452adaae63c9d96e70b4f526ab2
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-03-01 23:47:27 +0545

    added assi 5

---
+++ Assi_5/src/Triangle.java
@@ -0,0 +1,26 @@
+public class Triangle {
+    int a,b,c;
+    Triangle(){
+        a=3;
+        b=4;
+        c=5;
+    }
+    Triangle(int d, int e, int f){
+        a=d;
+        b=e;
+        c=f;
+    }
+    public void area() {
+        int s = (a + b + c) / 2;
+        s = (s * (s - a) * (s - b) * (s - c));
+        System.out.println("The area of the triangle is:" + s);
+    }
+    public void perimeters(){
+        int p =a+b+c;
+        System.out.println("The parameter is:"+p);
+    }
+
+
+
+
+}
