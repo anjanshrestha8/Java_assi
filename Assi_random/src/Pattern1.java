413247fd0f56e3800182142f153263626980b240
Anjan Shrestha <anjanshrestha@Anjans-MacBook-Air.local>
2023-03-09 11:39:05 +0545

    pattern question is solved

---
+++ Assi_random/src/Pattern1.java
@@ -0,0 +1,17 @@
+//        *****
+//        *****
+//        *****
+//        *****
+//        *****
+public class Pattern1 {
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++)
+        {
+            for(int j=0;j<=4;j++)
+            {
+                System.out.print("*"); // println le \n ko kam garxa so print matra use garne
+            }
+           System.out.println(" ");
+        }
+    }
+}
